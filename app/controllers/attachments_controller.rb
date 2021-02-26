502b55c16e998e01be2056268f717a852f16196b
Rhoxio <rhoxiodbc@gmail.com>
2021-02-26 07:51:47 -0800

    got delete functionality in for galleries

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,6 +1,25 @@
 class AttachmentsController < InheritedResources::Base
 
+  before_action :authenticate_user!
+
+  def destroy
+    @attachment = Attachment.find(params[:id])
+    gallery = @attachment.gallery
+
+    if gallery.user == current_user
+      @image = ActiveStorage::Attachment.find(params[:id])
+      @image.purge
+      @attachment.delete
+    end
+
+    redirect_to edit_gallery_path(gallery)
+  end
+
   private
+
+    def validate_ownership
+      # p params
+    end
 
     def attachment_params
       params.require(:attachment).permit(:caption, :photo)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :galleries
-  # resources :attachments
+  resources :attachments
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: "home#index"
